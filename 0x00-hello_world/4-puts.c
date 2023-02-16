6cf81cd90298abe7792a5a59a06f8e1e2c381b60
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-16 12:30:15 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - outputs a line of text using puts
+ * Return: 0 this means a success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
