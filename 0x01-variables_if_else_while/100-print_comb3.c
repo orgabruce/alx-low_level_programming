f1b1fd597c5f7e29f2a22129b401466a4f01f538
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-18 06:43:28 -0800

    100-print_comb3.c edit 4

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,7 +17,9 @@ int main(void)
 		{
 			putchar ('0' + i);
 			putchar ('0' + j);
-			if ((i < 8) && (j < 9))
+			if ((i == 8) && (j == 9))
+				;
+			else
 			{
 			putchar (',');
 			putchar (' ');
