7957af6da4629c017fb2a1752a4b2828d5214d10
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-17 22:00:02 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits
+ * Return: (0) sucess
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar ('0' + i);
+		i++;
+	}
+
+	putchar ('\n');
+
+	retun(0);
+}
