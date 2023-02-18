aeb422710f127678ad4a13cf5ecc5a88dfdd9392
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-17 20:51:16 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program to print lower case alphabets
+ * Return: (0) means success
+ */
+
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar (x);
+		x++;
+	}
+
+	putchar ("\n");
+
+	return (0);
+}
