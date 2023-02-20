07583ad320cf7b36d216e8974647060212fe3a48
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-20 10:31:01 -0800

    102-print_comb5.c edit addition

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,5 +7,40 @@
 
 int main(void)
 {
+	int c1;
 
+	for (c1 = 0; c1 <= 9; c1++)
+	{
+		int c2;
+
+		for (c2 = c1; c2 <= 9; c2++)
+		{
+			int c3;
+
+			for (c3 = 0; c3 <= 9; c3++)
+			{
+				int c4;
+
+				for (c4 = c3 + 1; c4 <= 9; c4++)
+				{
+					putchar ('0' + c1);
+					putchar ('0' + c2);
+					putchar (' ');
+					putchar ('0' + c3);
+					putchar ('0' + c4);
+
+					if ((c1 != 9) && (c2 != 8) && (c3 != 9) && (c4 != 9))
+					{
+						putchar (',');
+						putchar (' ');
+
+					}
+				}
+			}
+		}
+	}
+
+	putchar ('\n');
+
+	return (0);
 }
