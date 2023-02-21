92cdaddf20b7263822a89a2ece6662bd1c949d2a
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-21 10:35:09 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - checks sign of a number
+ * @n: input number
+ * Return: 1 if positive, 0 if zero and -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
