4368d4a57170825e51838a16ea4cf35147cc3fd5
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-22 06:01:07 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers starting from n to 98
+ * @n: start number
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		int i = n;
+
+		while (i <= 98)
+		{
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+			else
+			{
+				printf("%d ,", i);
+			}
+			i++;
+		}
+	}
+	else if (n > 98)
+	{
+		int i = n;
+
+		while (i >= 98)
+		{
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+			else
+			{
+				printf("%d ,", i);
+			}
+		}
+	}
+}
+
