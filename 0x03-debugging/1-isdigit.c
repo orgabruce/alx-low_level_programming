1aa38682b561d09fb5a078528d2d9896711404e2
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-23 10:24:51 -0800

    1-isdigit.c commit 1

---
+++ 0x03-debugging/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isdigit - checks if input is a digit
+* @c: input variable
+* Return: 1 if digit else 0
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 1 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
