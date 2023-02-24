86f328c735c8cd026a6059a9b4c59d9ff8f8c0d5
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-24 10:22:19 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - function that draws a square
+ * @size: input variable
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i = 0;
+
+		while (i < size)
+		{
+			int j = 0;
+
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
