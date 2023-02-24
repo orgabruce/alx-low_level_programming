4f8d3e92b24c1cb4bd4edb2ae52c373ad48711a4
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-24 10:41:24 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <std.io>
+
+/**
+ * main - the Fizz-Buzz Test
+ * Return: 0 means success
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
