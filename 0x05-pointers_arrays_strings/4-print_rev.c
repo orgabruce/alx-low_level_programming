a30ec92636c750abf2a0f653ba3070812518706c
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-27 20:18:05 -0800

    4-print_reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - prints input string in reverse
+ * @s: input string
+ * return: 0 means success
+ */
+
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	s--;
+
+	int i = l;
+
+	while (i > 0)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
