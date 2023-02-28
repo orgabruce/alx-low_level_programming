48cb15ec651b0b07201adea57f542e65495c7149
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-02-28 04:37:43 -0800

    8-print_array.c commit 6

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,15 @@ void print_array(int *a, int n)
 
 	while (i < n)
 	{
-		printf("%d, ", a[i]);
+		if (i == (n -1))
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+
 		i++;
 	}
 
