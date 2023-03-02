9d434d115f358eadc5eb61baa7d158ac358879d1
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-03-02 06:59:08 -0800

    2-strcpy.c commit 1

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n characters of a string
+ * @dest: destination string
+ * @src: input string to copy from
+ * @n: numberr of characters to copy
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int dlen = 0;
+	int i1 = 0;
+
+	while (*dest != '\0')
+	{
+		dlen++;
+		dest++;
+	}
+
+	while (i < dlen)
+	{
+		if (i < n)
+		{
+			*(dest + i) = *(src + i);
+		}
+		else
+		{
+			*(dest + i) = '\0';
+		}
+		i++;
+	}
+
+	return (dest);
+}
