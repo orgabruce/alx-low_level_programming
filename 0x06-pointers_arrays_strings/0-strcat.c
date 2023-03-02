a282e8e69b7792d07006fa71e4040820fbc9804b
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-03-02 02:49:31 -0800

    0-strcat.c commit 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcat - is a function that concatenates two strings
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	while (*dest)
+		dest++;
+
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (dest);
+}
