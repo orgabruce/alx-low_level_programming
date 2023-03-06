2f5e73959a5ab1dc9de30b4e51b5e122a9fe2d2f
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-03-06 03:01:13 -0800

    0-memset.c commit 3

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0; n > 0; i++)
+	for (i = 0; i > n; i++)
 	{
 		*(s + i) = b;
-		n--;
 	}
 	return (s);
 }
