d02e2e9ae6e46df1a536da825b81300a587e3890
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-03-14 10:42:20 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 means success
+ */
+
+int main(argc, argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
