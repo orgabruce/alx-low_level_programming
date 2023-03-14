e05662abcf14da5a6524382e3d5b6a0bcb6452b6
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-03-14 10:59:06 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: 0 means success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
