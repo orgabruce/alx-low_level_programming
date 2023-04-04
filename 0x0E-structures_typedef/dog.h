3500dcd943158c3b92fb3675e76b52d2150522ff
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-04-03 18:07:01 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct for dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: Some detailed description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
