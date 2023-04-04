afe3da32c3801c5ce221416111cdb0eb3e41fe79
Bensille Bruce Aburinya <brucebenille@gmail.com>
2023-04-03 18:28:29 -0700

    dog.h 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,8 +6,7 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
- * Description: Some detailed description
- */
+*/
 
 struct dog
 {
@@ -16,9 +15,6 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - typedef for struct dog
- */
 
 typedef struct dog dog_t;
 
@@ -26,7 +22,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
